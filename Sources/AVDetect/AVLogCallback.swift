b522e89e312c117483dbd0dbe1c59ad046b81b32
YiLiu <939488764@qq.com>
2020-03-25 15:31:56 +0800

    fff

--- Sources/AVDetect/AVLogCallback.swift
+++ Sources/AVDetect/AVLogCallback.swift
@@ -1,20 +1,20 @@
 import CFFmpeg
 
 func callback(ptr: UnsafeMutableRawPointer?, level: Int32, fmt: UnsafePointer<Int8>?, ars: CVaListPointer) -> Void {
-    if let data = fmt {
-        let format = String.init(cString: data)
-        var buffer: UnsafeMutablePointer<Int8>? = nil
-        let str = format.withCString { cString -> String? in
-            guard vasprintf(&buffer, cString, ars) != 0 else {
-                return nil
-            }
-            return String(validatingUTF8: buffer!)
-        }
-        guard let log = str else { return }
-        if let closure = avComplete {
-            closure(log)
-        }
-    }
+//    if let data = fmt {
+//        let format = String.init(cString: data)
+//        var buffer: UnsafeMutablePointer<Int8>? = nil
+//        let str = format.withCString { cString -> String? in
+//            guard vasprintf(&buffer, cString, ars) != 0 else {
+//                return nil
+//            }
+//            return String(validatingUTF8: buffer!)
+//        }
+//        guard let log = str else { return }
+//        if let closure = avComplete {
+//            closure(log)
+//        }
+//    }
 }
 
 var avComplete: ((String) -> Void)? = nil
